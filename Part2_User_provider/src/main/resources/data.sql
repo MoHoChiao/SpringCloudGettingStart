e879ea8c4fefa81992a28e428f4171388c882428
hungw <hungweiliu543@gmail.com>
2017-08-08 18:05:41 +0800

    Add Part2 MicroService Provider

---
+++ Part2_User_provider/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into user (id, username, name, age, balance) values (1, 'account1', 'LeoLiu', 20, 100.00);
+insert into user (id, username, name, age, balance) values (2, 'account2', 'TroyChen', 28, 180.00);
+insert into user (id, username, name, age, balance) values (3, 'account3', 'MichaelLin', 32, 280.00);
+insert into user (id, username, name, age, balance) values (4, 'account4', 'DeanLin', 32, 280.00);
+insert into user (id, username, name, age, balance) values (5, 'account5', 'RaelLin', 32, 280.00);
